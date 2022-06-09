31d878f05e304243c7689f60eb43f33b435202ce
Mike Lawrence <mjalawrence1@gmail.com>
2022-06-09 09:02:23 +0100

    stretch2

--- index.js
+++ index.js
@@ -26,12 +26,19 @@ function responseFormat (status, message, data) {
 
 }
 
-app.get('/products', async (req, res) => {
-    const connection = await getDb()
-    const basic_items = await connection.query('SELECT `id`, `image`, `title`, `price` FROM `products`')
-    res.json(basic_items)
-})
 
+// - Start out by creating an endpoint/route should be `/products` and this should return JSON containing every product the database.
+// - This route should return the following fields for every product:
+//- ID - image - title - price
+// app.get('/products', async (req, res) => {
+//     const connection = await getDb()
+//     const basic_items = await connection.query('SELECT `id`, `image`, `title`, `price` FROM `products`')
+//     res.json(basic_items)
+// })
+
+//- The next one should be an endpoint/route for `products/id`
+//     - This route should return only a single product (based on it's ID)
+//      - Should return every field from the DB table
 app.get('/products/:id', async (req, res) => {
     const connection = await getDb()
 
@@ -41,6 +48,7 @@ app.get('/products/:id', async (req, res) => {
     res.json({items})
 })
 
+// - Create a POST route for adding a new product
 app.post('/products/add', async (req, res) => {
     const connection = await getDb()
     const items = await connection.query('INSERT INTO `products` (`title`, `price`, `image`, `category_id`, `category`, `character_id`, `character`, `description`, `image2`, `image3`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [req.body.title, req.body.price, req.body.image, req.body.category_id, req.body.category, req.body.character_id, req.body.character, req.body.description, req.body.image2, req.body.image3])
@@ -48,6 +56,7 @@ app.post('/products/add', async (req, res) => {
     res.json(responseFormat('200', 'Upload complete', items))
 })
 
+// - Create a PUT route for updating an existing product
 app.put('/products/update/:id', async (req, res) => {
     const connection = await getDb()
     const items = await connection.query('UPDATE `products` SET `title` =?,`price` =?, `image` =?, `category_id` =?, `category` =?, `character_id` =?, `character` =?, `description` =?, `image2` =?, `image3` =? WHERE `id` =?',[req.body.title, req.body.price, req.body.image, req.body.category_id, req.body.category, req.body.character_id, req.body.character, req.body.description, req.body.image2, req.body.image3, req.params.id])
@@ -55,6 +64,7 @@ app.put('/products/update/:id', async (req, res) => {
     res.json(responseFormat('200', req.body.title + ' has been updated', items))
 })
 
+// - Create a DELETE route for removing an existing product
 app.put('/products/delete/:id', async (req, res) => {
     const connection = await getDb()
     const items = await connection.query('UPDATE `products` SET `deleted` = 1 WHERE `id` =?',[ req.params.id])
@@ -62,7 +72,51 @@ app.put('/products/delete/:id', async (req, res) => {
     res.json(responseFormat('200', req.body.title + ' has been deleted', items))
 })
 
+// app.get('/products/:category', async (req, res) => {
+//     const connection = await getDb()
+//     // const category =
+//     const items = await connection.query('SELECT * FROM `products` WHERE `category` = ?', [req.params.category])
+//     res.json(responseFormat('200', req.params.category + ' are displayed', items))
+// })
+
+app.get('/products', async (req, res) => {
+    const connection = await getDb()
+    let where_stmt = ''
+    let bound_params = []
+
+    if (req.query.category || req.query.character) {
+        where_stmt += ' WHERE'
+    }
+
+    if (req.query.category) {
+        where_stmt += ' `category` = ?'
+        bound_params.push(req.query.category)
+    }
+
+    if (req.query.character && req.query.category) {
+        where_stmt += ' AND'
+
+    }
+
+    if (req.query.character) {
+        where_stmt += ' `character` = ?'
+        bound_params.push(req.query.character)
+    }
+
+    let result = await connection.query('SELECT `title`, `price`, `image`, `category_id`, `category`, `character_id`, `character`, `description`, `image2`, `image3` FROM `products`' + where_stmt, bound_params)
+
+    res.json(responseFormat('200', 'filtered selection displayed', result))
+})
+
+
 app.listen(port)
+
+
+
+
+
+
+
 
 //[Exercise] RESTful e-commerce store API
 //
